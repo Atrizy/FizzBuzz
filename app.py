2eedbbc8fc19cf98350af98fe6c922c299f43632
Atrizy <evankeeping21@gmail.com>
2022-01-22 18:51:16 -0330

    finished it

---
+++ app.py
@@ -0,0 +1,26 @@
+def fizz_num(num):
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(str(num))
+
+fizz_num(1)
+fizz_num(2)
+fizz_num(3)
+fizz_num(4)
+fizz_num(5)
+fizz_num(6)
+fizz_num(7)
+fizz_num(8)
+fizz_num(9)
+fizz_num(10)
+fizz_num(11)
+fizz_num(12)
+fizz_num(13)
+fizz_num(14)
+fizz_num(15)
+
